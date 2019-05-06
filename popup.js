c56f75fb5ec8ef969a156bc6b7023f9a2fcd415a
AntoninRoy <anto.roy79@gmail.com>
2019-05-06 15:13:58 +0200

    Creation de l'interface de connexion et mecanisme de connexion automatique si connexion il y a moins d'une heure

--- popup.js
+++ popup.js
@@ -2,17 +2,79 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+var MINUTES_55 = 550 * 60 * 1000; 
+var SECONDES_20 = 20 * 1000; 
+
+
 document.addEventListener('DOMContentLoaded', () => {
-  //button listener
-    document.getElementById("btn_connection").addEventListener("click", function() { 
-      //get current tabs
-      chrome.tabs.query({'active': true, 'lastFocusedWindow': true}, function (tabs) {
-      });
+  console.log("c'est parti");
+
+  var connection = document.getElementById("connection");
+  var connected = document.getElementById("connected");
+
+    chrome.storage.local.get(['timespamp_connect'], function(result) {
+      if(((new Date) - result.timespamp_connect) < SECONDES_20){
+        connection.style.display = 'none';
+        connected.style.display = 'flex';
+      }
     });
 
-    //inject du javascript dans la page actuelle
-    chrome.tabs.executeScript( {file: 'content_script.js'});
-});
+    //button listener
+    document.getElementById("btn_connection").addEventListener("click", function() {
+
+        var input_login = document.getElementById("login").value;
+        var input_password = document.getElementById("password").value;
+
+        connect(input_login,input_password);
+
+        //inject du javascript dans la page actuelle
+        chrome.tabs.executeScript( {file: 'content_script.js'});
+    });
+  });
+function connect(login,password){
+  var error = document.getElementById("error");
+  if(checkString(login) && checkString(password)){
+    var loader = document.getElementById("loader");
+    var connection = document.getElementById("connection");
+    var connected = document.getElementById("connected");
+
+
+    loader.style.display = 'block';      
+    connection.style.display = 'none';  
+
+    var xhr = new XMLHttpRequest();
+    xhr.open("GET", chrome.extension.getURL('http://dummy.restapiexample.com/api/v1/employees'), true);
+    xhr.onreadystatechange = function() {
+      if (xhr.readyState == 4) {
+        if(login.includes("1")){
+          loader.style.display = 'none';  
+          connected.style.display = 'flex';
+
+          chrome.storage.local.set({ "timespamp_connect": Date.now() }, null);
+          chrome.storage.local.set({ "token": Date.now() }, null);
+        }else{
+          loader.style.display = 'none';  
+          connection.style.display = 'flex';
+          error.textContent="Login ou Mot de passe incorrect.";
+        }
+
+      }
+    }
+    xhr.send();
+  }else{
+    error.textContent="Certains champs sont vides.";
+  }
+    
+}
+
+function checkString(text){
+  if(!text)
+    return false;
+  if(text==="")
+    return false;
+
+  return true;
+}
 
 
 
