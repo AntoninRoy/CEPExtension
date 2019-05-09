1e593e9c1cdb5e90d1cff260be621e3c020e56de
AntoninRoy <anto.roy79@gmail.com>
2019-05-09 16:07:09 +0200

    Suppression de tous le système de connexion car finalement non utilisé, implémentation de la récupération des adhérents via une page administration sur la Web Application

--- content_script.js
+++ content_script.js
@@ -1,10 +1,18 @@
-chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-    console.log("Message reçu de la part du plugin : "+request);
-    switch(request.command) {
-        case "hello" :
-            sendResponse("ok");
-            break;    
+chrome.runtime.onMessage.addListener(function(data, sender, sendResponse) {
+    console.log("chrome.runtime.onMessage: "+data.command);
+    switch(data.command) {
+        case "copy_licence_holders" :
+            console.log("La je copie");
+            sendResponse({
+                response: {
+                    "dzqdzqdqz" : "dzqdqzdqz",
+                    "dzdzq" : "dsqdqzd"
+                }
+            });
+            break; 
         default:
-            sendResponse(null);
+        sendResponse({
+            response: "Error"
+        });
     }
 });
--- popup.js
+++ popup.js
@@ -2,122 +2,47 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-var MINUTES_55 = 550 * 60 * 1000; 
-var SECONDES_20 = 20 * 1000; 
-
-
 document.addEventListener('DOMContentLoaded', () => {
+  //button listener
+    document.getElementById("btn_alert").addEventListener("click", function() { 
+      //get current tabs
+      paste();
+    });
 
-  //test if user is already connected
-  chrome.storage.local.get(['timespamp_connect'], function(result) {
-    if(((new Date) - result.timespamp_connect) < SECONDES_20){
-      showLoader();
-      getMembers();
-    }
-  });
 
-  //button listener connection
-  document.getElementById("btn_connection").addEventListener("click", function() {
-      var input_login = document.getElementById("login").value;
-      var input_password = document.getElementById("password").value;
+    document.getElementById("btn_copy").addEventListener("click", function() { 
+      copy();
+    });
 
-      connect(input_login,input_password);
 
-      //inject du javascript dans la page actuelle
-      chrome.tabs.executeScript( {file: 'content_script.js'});
-  });
-
+    //inject du javascript dans la page actuelle
+    chrome.tabs.executeScript( {file: 'content_script.js'});
 });
 
 
-function connect(login,password){
-  var error = document.getElementById("error");
-  if(checkString(login) && checkString(password)){
-    showLoader();
-
-    $.ajax({
-      url: "http://dummy.restapiexample.com/api/v1/employees",
-      type: 'GET',
-      dataType: 'json',             
-      contentType: "application/json",
-      success: function (response) {
-          if(login.includes("1")){
+function copy() {
+  chrome.tabs.query({'active': true, 'lastFocusedWindow': true}, function (tabs) {
+      var data = new Object();
+      data["command"] = "copy_licence_holders";
+      // request content_script to retrieve title element innerHTML from current tab
+      chrome.tabs.sendMessage(tabs[0].id, data, null, function(obj) {
+         // alert("copy_licence_holders: "+ obj.response);
           
-            chrome.storage.local.set({ "timespamp_connect": Date.now() }, null);
-            chrome.storage.local.set({ "token": Date.now() }, null);
-  
-            getMembers();
-            
-            
-          }else{
-            showConnection()
-            error.textContent="Login ou Mot de passe incorrect.";
-          }
-        }
-    });
-  }else{
-    error.textContent="Certains champs sont vides.";
-  }
-    
-}
-
-function getMembers(){
-  var select = document.getElementById("member-select");
-  $.ajax({
-    url: "http://dummy.restapiexample.com/api/v1/employees",
-    type: 'GET',
-    dataType: 'json',             
-    contentType: "application/json",
-    success: function (response) {
-      if(response.erreur != null ){
-      }else{
-        response.forEach(function(element) {
-          //users[element.id] = element;
-          select.options[select.options.length] = new Option(element.employee_name, element.id);
-        });
-        showConnected();
-      }
-    }
+          chrome.storage.sync.set({ "users": obj.response }, function(){
+          
+          });
+      });
   });
 }
 
-function showLoader(){
-  hideAll();
-  var loader = document.getElementById("loader");
-  loader.style.display = 'block';      
+function paste(){
+  chrome.tabs.query({'active': true, 'lastFocusedWindow': true}, function (tabs) {
+    chrome.storage.sync.get(/* String or Array */["users"], function(items){
+
+      alert(items.users);
+      
+    });
+  
+  });
 }
-function showConnected(){
-  hideAll();
-  var connected = document.getElementById("connected");
-  connected.style.display = 'flex';
-}
-
-function showConnection(){
-  hideAll();
-  var connection = document.getElementById("connection");
-  connection.style.display = 'flex';
-
-}
-
-function hideAll(){
-  var connection = document.getElementById("connection");
-  connection.style.display = 'none';
-
-  var connected = document.getElementById("connected");
-  connected.style.display = 'none';
-
-  var loader = document.getElementById("loader");
-  loader.style.display = 'none'; 
-}
-function checkString(text){
-  if(!text)
-    return false;
-  if(text==="")
-    return false;
-
-  return true;
-}
-
-
-
 
