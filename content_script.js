a66df2b13b4a7b999250330094efaebea5ae2e1e
AntoninRoy <anto.roy79@gmail.com>
2019-05-10 16:51:41 +0200

    Finalsiation de la copie, Injection des données a partir de la séléction d'un adhérent, Front du plugin

--- content_script.js
+++ content_script.js
@@ -1,18 +1,113 @@
-chrome.runtime.onMessage.addListener(function(data, sender, sendResponse) {
+chrome.runtime.onMessage.addListener(async function(data, sender, sendResponse) {
     console.log("chrome.runtime.onMessage: "+data.command);
     switch(data.command) {
         case "copy_licence_holders" :
             console.log("La je copie");
+            var text =  document.getElementById("competiteur").textContent;
             sendResponse({
-                response: {
-                    "dzqdzqdqz" : "dzqdqzdqz",
-                    "dzdzq" : "dsqdqzd"
-                }
+                response: JSON.parse(text)
             });
             break; 
+        case "paste_licence_holder" :
+           await getVilles();
+           inject(data);
+
+
+           sendResponse({
+            response: "Adhérent copié avec succès."
+            });
+    
+            break;
+
         default:
-        sendResponse({
-            response: "Error"
-        });
+            sendResponse({
+                response: "Error"
+            });
     }
 });
+
+function getVilles() {
+    return new Promise(function (resolve, reject) {
+        var xhr = new XMLHttpRequest();
+        xhr.open("GET", "https://datanova.legroupe.laposte.fr/api/records/1.0/search/?dataset=laposte_hexasmal&sort=code_postal&facet=nom_de_la_commune&facet=code_postal&refine.code_postal=79000", true);
+        xhr.onload = function () {
+            var status = xhr.status;
+            if (status == 200) {
+                let setArr = new Set();
+                const villes = JSON.parse(xhr.responseText)
+                //console.log(villes);
+                villes.records.forEach(element => {
+                    console.log(element.fields.nom_de_la_commune);
+                    setArr.add(element.fields.nom_de_la_commune);
+                }); 
+                console.log("fini");
+                resolve(setArr);            
+            }
+            else if (status == 400) {
+                reject(status);
+            }
+            else {
+                reject(status);
+            }
+            return true;
+        };
+        xhr.send();
+    });
+}
+
+function inject(data){
+            //console.log(data.user);
+            console.log("Les informations du licencié sont ajoutées ");
+            var jform_ACT_nom =  document.getElementById("jform_ACT_nom").value=data.user.lastName;
+            var jform_ACT_prenom =  document.getElementById("jform_ACT_prenom").value= data.user.firstName;
+            var jform_ACT_adr1 =  document.getElementById("jform_ACT_adr1").value=data.user.addressRep1;
+            var jform_ACT_adr2 =  document.getElementById("jform_ACT_adr2").value="";
+            var jform_ACT_adr3 =  document.getElementById("jform_ACT_adr3").value="";
+            //le pays, select option
+            var jform_ACT_cp =  document.getElementById("jform_ACT_cp").value=data.user.zipCodeRep1;
+            //ville, select option (dynamique)
+            var jform_ACT_dateNaissance =  document.getElementById("jform_ACT_dateNaissance").value=data.user.birthDate;
+            var jform_ACT_tel1 =  document.getElementById("jform_ACT_tel1").value=data.user.phoneRep1;
+            var jform_ACT_tel2 =  document.getElementById("jform_ACT_tel2").value=data.user.phoneRep2;
+            var jform_ACT_mail =  document.getElementById("jform_ACT_mail").value=data.user.emailRep1;
+            var jform_ACT_nomEpouse =  document.getElementById("jform_ACT_nomEpouse").value="";
+            var jform_ACT_num_licence =  document.getElementById("jform_ACT_num_licence").value="";
+            //sexe, select option
+            //saison, select option
+            //discipline pratiquée. x3 Select option 
+            //sous discipline select option
+            var jform_AAD_rl1_nom =  document.getElementById("jform_AAD_rl1_nom").value=data.user.firstNameRep1;
+            var jform_AAD_rl1_tel =  document.getElementById("jform_AAD_rl1_tel").value=data.user.phoneRep1;
+            var jform_AAD_rl1_mail =  document.getElementById("jform_AAD_rl1_mail").value=data.user.emailRep1;
+            var jform_AAD_rl2_nom =  document.getElementById("jform_AAD_rl2_nom").value=data.user.firstNameRep2;
+            var jform_AAD_rl2_tel =  document.getElementById("jform_AAD_rl2_tel").value=data.user.phoneRep2;
+            var jform_AAD_rl2_mail =  document.getElementById("jform_AAD_rl2_mail").value=data.user.emailRep2;
+            //select radio Certificat médical de santé
+            //select radio Questionnaire de santé
+            //Select option droit à l'image
+           var jform_AAD_date_inscription =  document.getElementById("jform_AAD_date_inscription").value=data.user.jform_AAD_date_inscription;
+            //select list nationalité
+           var jform_AAD_CM_date =  document.getElementById("jform_AAD_CM_date").value=data.user.registrationDate;
+            //Radio input Bordereau assurance FFG
+            //Radio input Bloquer les envois par email
+            //Radio input Licencié autre club
+            //role dans l'association x5
+            var jform_AAD_comment =  document.getElementById("jform_AAD_comment").value="";
+            var jform_18086_0171002 =  document.getElementById("jform_18086_0171002").value="";
+            var jform_18086_0171003 =  document.getElementById("jform_18086_0171003").value="";
+            var jform_18086_0171004 =  document.getElementById("jform_18086_0171004").value="";
+            var jform_18086_0171005 =  document.getElementById("jform_18086_0171005").value="";
+            var jform_18086_0171006 =  document.getElementById("jform_18086_0171006").value="";
+            var jform_18086_0171007 =  document.getElementById("jform_18086_0171007").value="";
+            var jform_18086_0171008 =  document.getElementById("jform_18086_0171008").value="";
+            var jform_18086_0171009 =  document.getElementById("jform_18086_0171009").value="";
+            var jform_18086_0171010 =  document.getElementById("jform_18086_0171010").value="";
+            var jform_18086_0171011 =  document.getElementById("jform_18086_0171011").value="";
+            var jform_18086_0171012 =  document.getElementById("jform_18086_0171012").value="";
+            var jform_18086_0171013 =  document.getElementById("jform_18086_0171013").value="";
+            var jform_18086_0171014 =  document.getElementById("jform_18086_0171014").value="";
+            var jform_18086_0171015 =  document.getElementById("jform_18086_0171015").value="";
+
+}
+
+
--- popup.js
+++ popup.js
@@ -1,20 +1,50 @@
 // Copyright (c) 2014 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
+const days = ["Dimanche","Lundi","Mardi","Mercredi","Jeudi","Vendredi","Samedi"];
+const months = ["Décembre","Janvier","Fevrier","Mars","Avril","Mai","Juin","Juillet","Aout","Septembre","Octobre","Novembre"];
 
-document.addEventListener('DOMContentLoaded', () => {
+
+document.addEventListener('DOMContentLoaded',  () => {
+  chrome.storage.local.get(["last_update"], function(items){
+    if(items.last_update != null){
+      
+      const last_update_date = new Date(items.last_update);
+     
+      document.getElementById("last_update").textContent = 'Dernière initialisation : '+ days[last_update_date.getDay()]+ " "+ last_update_date.getDate() +" " + months[last_update_date.getDay()]+", "+ last_update_date.getHours()+"h"+last_update_date.getMinutes();
+    }else{
+      document.getElementById("last_update").textContent = "Dernière initialisation : Jamais";
+
+    }
+
+  });
+
+  
+  chrome.storage.local.get(["users"], function(items){
+    if(items.users != null){
+      var select = document.getElementById("member-select");
+
+      for (let i = 0; i < items.users.length; i++) {
+        const element = items.users[i];
+        select.options[select.options.length] = new Option(element.firstName + " " + element.lastName, element.id);
+      }
+    }
+   
+
+  });
+  
+
   //button listener
     document.getElementById("btn_alert").addEventListener("click", function() { 
-      //get current tabs
-      paste();
+      document.getElementById("errorPaste").textContent = "Copie en cours ...";
+       paste();
     });
 
 
     document.getElementById("btn_copy").addEventListener("click", function() { 
+      document.getElementById("errorInit").textContent = "Initialisation en cours ...";
       copy();
     });
-
-
     //inject du javascript dans la page actuelle
     chrome.tabs.executeScript( {file: 'content_script.js'});
 });
@@ -26,23 +56,66 @@ function copy() {
       data["command"] = "copy_licence_holders";
       // request content_script to retrieve title element innerHTML from current tab
       chrome.tabs.sendMessage(tabs[0].id, data, null, function(obj) {
-         // alert("copy_licence_holders: "+ obj.response);
+          if(obj == undefined|| obj.response == undefined || obj.response.length < 1 || obj.response.length ==undefined){
+            document.getElementById("errorInit").textContent = "Impossible de copier les données.";
+          }else{
+            chrome.storage.local.set({ "users": obj.response }, null);
+            chrome.storage.local.set({ "last_update": Date.now() }, null);
+            var select = document.getElementById("member-select");
+  
+            for (let i = 0; i < obj.response.length; i++) {
+              const element = obj.response[i];
+              select.options[select.options.length] = new Option(element.firstName + " " + element.lastName, element.id);
+            }
+            const last_update_date = new Date();
+            document.getElementById("last_update").textContent = 'Dernière initialisation : '+ days[last_update_date.getDay()]+ " "+ last_update_date.getDate() +" " + months[last_update_date.getDay()]+", "+ last_update_date.getHours()+"h"+last_update_date.getMinutes();
+            document.getElementById("errorInit").textContent = "Données copiées avec succès.";
+          }
+         
+
           
-          chrome.storage.sync.set({ "users": obj.response }, function(){
-          
-          });
       });
   });
 }
 
-function paste(){
-  chrome.tabs.query({'active': true, 'lastFocusedWindow': true}, function (tabs) {
-    chrome.storage.sync.get(/* String or Array */["users"], function(items){
+  function paste(){
+    var select = document.getElementById("member-select");
+    findInUsers(select.options[select.selectedIndex].value);
+}
 
-      alert(items.users);
-      
-    });
-  
+ function findInUsers(id){
+   chrome.storage.local.get(["users"], function(items){
+     let flag =false;
+     let user;
+
+    for (let i = 0; i < items.users.length && !flag; i++) {
+      const element = items.users[i];
+      if(element.id == id){
+       flag =true;
+       user = element;
+      }
+    }
+
+    if(!flag)
+      inccorectValue("Vous devez séléctionner un adhérent ! ");
+    else
+      sendUser(user);
   });
+}
+
+function sendUser(user){
+    //alert(items.users[0].firstName);
+    var data = new Object();
+    data["command"] = "paste_licence_holder";
+    data["user"] = user;
+    chrome.tabs.query({'active': true, 'lastFocusedWindow': true}, function (tabs) {
+      chrome.tabs.sendMessage(tabs[0].id, data, null, function(obj) {
+        document.getElementById("errorPaste").textContent = "Adhérent copié avec succès.";
+      });
+    });
+}
+
+function inccorectValue(error){
+  document.getElementById("errorPaste").textContent = error;
 }
 
