01262df483419b503e0978754f711d310c4b4313
AntoninRoy <anto.roy79@gmail.com>
2019-06-03 17:17:02 +0200

    comment on code

--- background.js
+++ background.js
@@ -1,22 +1,34 @@
+//Le background.js est un fichier qui s'execute en arrière plan même quand le plugin n'est pas ouvert. 
+//On peut donc modifier les variable du google storage et receptionner des messages sans que le plugin soit ouvert.
+
+//Listener pour les message (les messages permettent de communiquer entre l'extension chrome et le javascript de la page)
 chrome.runtime.onMessage.addListener(function(data, sender, sendResponse) {
+    //switch afin d'executer le traitement voulu en fonction du parametre reçu dans le message.
     switch(data.command){
+        //message reçu lorsque l'envoi du formulaire a été envoyé avec succès.
         case "user_validate" :
             user_validate(data.actual_user);
-            console.log(data.actual_user);
             break;
+        
+        //message reçu après une synchronisation réussi. 
         case "clean" : 
             chrome.storage.local.set({ "to_sync_users": null }, null);
             break;
+
+        default : 
     }
 });
 
-
-function user_validate(actual_user){
+/**
+ * Fonction qui enlève un adhérent de la liste "a copier"
+ * @param {*} user 
+ */
+function user_validate(user){
     chrome.storage.local.get(["to_sync_users","to_paste_users"], function(items){
         
         if(items.to_sync_users == null || items.to_sync_users.length == 0){
             //create to_sync_users array
-            const newAdherent = JSON.parse(actual_user);
+            const newAdherent = JSON.parse(user);
             var newto_sync_users = [
                 newAdherent
             ];
@@ -38,14 +50,14 @@ function user_validate(actual_user){
             var to_paste_users = items.to_paste_users;
             //forEach loop to test if the adherent is not already in the to_sync_users array
             let flag = true;
-            const newAdherent = JSON.parse(actual_user);
+            const newAdherent = JSON.parse(user);
             newto_sync_users.forEach(function (element) {
                 if(element.id == newAdherent.id){
                     flag=false;
                 }
             });
 
-            var to_paste_to_paste_users = items.to_paste_users;
+
             for(var i = 0; i < to_paste_users.length; i++)
             {
                 const element = to_paste_users[i];
--- content_script.js
+++ content_script.js
@@ -4,120 +4,196 @@ Une erreur dans le javascript de la page cible empechera le javascript injecté 
 */
 
 
-if(document.getElementById("first_open") != null){
+if (document.getElementById("first_open") != null) {
     console.log("Existe déja !");
-    
-}else{
+
+} else {
     console.log("n'existe pas  !");
     var body = document.getElementsByTagName('body')[0];
     var a = document.createElement('div');
     a.setAttribute("id", "first_open");
     body.after(a);
 
-    if(document.getElementById("btn_save_close") != undefined){
-        document.getElementById("btn_save_close").addEventListener("click", function (){
-            if(document.getElementById("acteur-form").checkValidity()){
+    if (document.getElementById("btn_save_close") != undefined) {
+        document.getElementById("btn_save_close").addEventListener("click", function () {
+            if (document.getElementById("acteur-form").checkValidity()) {
                 console.log("VALID");
-                if(localStorage.getItem('actual_user') != null){
-                    chrome.runtime.sendMessage(chrome.runtime.id,{command : "user_validate", actual_user : localStorage.getItem('actual_user')});
+                if (localStorage.getItem('actual_user') != null) {
+                    chrome.runtime.sendMessage(chrome.runtime.id, { command: "user_validate", actual_user: localStorage.getItem('actual_user') });
                     localStorage.setItem('actual_user', null);
                 }
-            }else{
+            } else {
                 console.log("NON VALIDE");
                 //pour les tests
-                if(true){
-                    if(localStorage.getItem('actual_user') != null){
-                        chrome.runtime.sendMessage(chrome.runtime.id,{command : "user_validate", actual_user : localStorage.getItem('actual_user')});
+                if (true) {
+                    if (localStorage.getItem('actual_user') != null) {
+                        chrome.runtime.sendMessage(chrome.runtime.id, { command: "user_validate", actual_user: localStorage.getItem('actual_user') });
                         localStorage.setItem('actual_user', null);
                     }
                 }
             }
-                
-            }, false);
+
+        }, false);
     }
 
 
-    if(document.getElementById("a") != null){
-        document.getElementById("a").addEventListener("click", function (){
-            chrome.runtime.sendMessage(chrome.runtime.id,{ command : "clean"});
+    if (document.getElementById("a") != null) {
+        document.getElementById("a").addEventListener("click", function () {
+            chrome.runtime.sendMessage(chrome.runtime.id, { command: "clean" });
         });
     }
 
 
-chrome.runtime.onMessage.addListener(OnMessageListener);
+    chrome.runtime.onMessage.addListener(OnMessageListener);
 
 
-    function inject(data){
-                //console.log(data.user);
-                console.log("Les informations du licencié sont ajoutées ");
-                var jform_ACT_nom =  document.getElementById("jform_ACT_nom").value=data.user.lastName;
-                var jform_ACT_prenom =  document.getElementById("jform_ACT_prenom").value= data.user.firstName;
-                var jform_ACT_adr1 =  document.getElementById("jform_ACT_adr1").value=data.user.addressRep1;
-                var jform_ACT_adr2 =  document.getElementById("jform_ACT_adr2").value="";
-                var jform_ACT_adr3 =  document.getElementById("jform_ACT_adr3").value="";
-                //le pays, select option
-                var jform_ACT_cp =  document.getElementById("jform_ACT_cp").value=data.user.zipCodeRep1;
-                //ville, select option (dynamique)
-                //var jform_ACT_dateNaissance =  document.getElementById("jform_ACT_dateNaissance").value=data.user.birthDate;
-                var jform_ACT_tel1 =  document.getElementById("jform_ACT_tel1").value=data.user.phoneRep1;
-                var jform_ACT_tel2 =  document.getElementById("jform_ACT_tel2").value=data.user.phoneRep2;
-                var jform_ACT_mail =  document.getElementById("jform_ACT_mail").value=data.user.emailRep1;
-                var jform_ACT_nomEpouse =  document.getElementById("jform_ACT_nomEpouse").value="";
-                var jform_ACT_num_licence =  document.getElementById("jform_ACT_num_licence").value="";
-                //sexe, select option
-                //saison, select option
-                //discipline pratiquée. x3 Select option 
-                //sous discipline select option
-                var jform_AAD_rl1_nom =  document.getElementById("jform_AAD_rl1_nom").value=data.user.firstNameRep1;
-                var jform_AAD_rl1_tel =  document.getElementById("jform_AAD_rl1_tel").value=data.user.phoneRep1;
-                var jform_AAD_rl1_mail =  document.getElementById("jform_AAD_rl1_mail").value=data.user.emailRep1;
-                var jform_AAD_rl2_nom =  document.getElementById("jform_AAD_rl2_nom").value=data.user.firstNameRep2;
-                var jform_AAD_rl2_tel =  document.getElementById("jform_AAD_rl2_tel").value=data.user.phoneRep2;
-                var jform_AAD_rl2_mail =  document.getElementById("jform_AAD_rl2_mail").value=data.user.emailRep2;
-                //select radio Certificat médical de santé
-                //select radio Questionnaire de santé
-                //Select option droit à l'image
-            var jform_AAD_date_inscription =  document.getElementById("jform_AAD_date_inscription").value=data.user.jform_AAD_date_inscription;
-                //select list nationalité
-            var jform_AAD_CM_date =  document.getElementById("jform_AAD_CM_date").value=data.user.registrationDate;
-                //Radio input Bordereau assurance FFG
-                //Radio input Bloquer les envois par email
-                //Radio input Licencié autre club
-                //role dans l'association x5
-                var jform_AAD_comment =  document.getElementById("jform_AAD_comment").value="";
-                var jform_18086_0171002 =  document.getElementById("jform_18086_0171002").value="";
-                var jform_18086_0171003 =  document.getElementById("jform_18086_0171003").value="";
-                var jform_18086_0171004 =  document.getElementById("jform_18086_0171004").value="";
-                var jform_18086_0171005 =  document.getElementById("jform_18086_0171005").value="";
-                var jform_18086_0171006 =  document.getElementById("jform_18086_0171006").value="";
-                var jform_18086_0171007 =  document.getElementById("jform_18086_0171007").value="";
-                var jform_18086_0171008 =  document.getElementById("jform_18086_0171008").value="";
-                var jform_18086_0171009 =  document.getElementById("jform_18086_0171009").value="";
-                var jform_18086_0171010 =  document.getElementById("jform_18086_0171010").value="";
-                var jform_18086_0171011 =  document.getElementById("jform_18086_0171011").value="";
-                var jform_18086_0171012 =  document.getElementById("jform_18086_0171012").value="";
-                var jform_18086_0171013 =  document.getElementById("jform_18086_0171013").value="";
-                var jform_18086_0171014 =  document.getElementById("jform_18086_0171014").value="";
-                var jform_18086_0171015 =  document.getElementById("jform_18086_0171015").value="";
+    function inject(data) {
+        //console.log(data.user);
+        console.log(data);
+        console.log("Les informations du licencié sont ajoutées ");
+        document.getElementById("jform_ACT_nom").value = data.user.lastName;
+        document.getElementById("jform_ACT_prenom").value = data.user.firstName;
+        document.getElementById("jform_ACT_adr1").value = data.user.addressRep1;
+        document.getElementById("jform_ACT_adr2").value = "";
+        document.getElementById("jform_ACT_adr3").value = "";
+
+        //le pays, select option
+        document.getElementById("jform_ACT_cp").value = data.user.zipCodeRep1;
+        //ville, select option (dynamique)
+        //var jform_ACT_dateNaissance =  document.getElementById("jform_ACT_dateNaissance").value=data.user.birthDate;
+        document.getElementById("jform_ACT_tel1").value = data.user.phoneRep1;
+        document.getElementById("jform_ACT_tel2").value = data.user.phoneRep2;
+        document.getElementById("jform_ACT_mail").value = data.user.emailRep1;
+        document.getElementById("jform_ACT_nomEpouse").value = "";
+        document.getElementById("jform_ACT_num_licence").value = "";
+        document.getElementById("jform_ACT_sexe").value = compare_select_value(document.getElementById("jform_ACT_sexe"), data.user.sex, true);
+        //saison, select option
+        //discipline pratiquée. x3 Select option 
+        //sous discipline select option
+        var jform_AAD_rl1_nom = document.getElementById("jform_AAD_rl1_nom").value = data.user.firstNameRep1;
+        var jform_AAD_rl1_tel = document.getElementById("jform_AAD_rl1_tel").value = data.user.phoneRep1;
+        var jform_AAD_rl1_mail = document.getElementById("jform_AAD_rl1_mail").value = data.user.emailRep1;
+        var jform_AAD_rl2_nom = document.getElementById("jform_AAD_rl2_nom").value = data.user.firstNameRep2;
+        var jform_AAD_rl2_tel = document.getElementById("jform_AAD_rl2_tel").value = data.user.phoneRep2;
+        var jform_AAD_rl2_mail = document.getElementById("jform_AAD_rl2_mail").value = data.user.emailRep2;
+        //select radio Certificat médical de santé
+        //select radio Questionnaire de santé
+        //Select option droit à l'image
+        var jform_AAD_date_inscription = document.getElementById("jform_AAD_date_inscription").value = data.user.jform_AAD_date_inscription;
+        document.getElementById("jform_ACT_pays").value = compare_select_value(document.getElementById("jform_ACT_pays"), data.user.nationality, false)
+
+        var jform_AAD_CM_date = document.getElementById("jform_AAD_CM_date").value = data.user.registrationDate;
+        //Radio input Bordereau assurance FFG
+        //Radio input Bloquer les envois par email
+        //Radio input Licencié autre club
+        //role dans l'association x5
+        var jform_AAD_comment = document.getElementById("jform_AAD_comment").value = "";
+        var jform_18086_0171002 = document.getElementById("jform_18086_0171002").value = "";
+        var jform_18086_0171003 = document.getElementById("jform_18086_0171003").value = "";
+        var jform_18086_0171004 = document.getElementById("jform_18086_0171004").value = "";
+        var jform_18086_0171005 = document.getElementById("jform_18086_0171005").value = "";
+        var jform_18086_0171006 = document.getElementById("jform_18086_0171006").value = "";
+        var jform_18086_0171007 = document.getElementById("jform_18086_0171007").value = "";
+        var jform_18086_0171008 = document.getElementById("jform_18086_0171008").value = "";
+        var jform_18086_0171009 = document.getElementById("jform_18086_0171009").value = "";
+        var jform_18086_0171010 = document.getElementById("jform_18086_0171010").value = "";
+        var jform_18086_0171011 = document.getElementById("jform_18086_0171011").value = "";
+        var jform_18086_0171012 = document.getElementById("jform_18086_0171012").value = "";
+        var jform_18086_0171013 = document.getElementById("jform_18086_0171013").value = "";
+        var jform_18086_0171014 = document.getElementById("jform_18086_0171014").value = "";
+        var jform_18086_0171015 = document.getElementById("jform_18086_0171015").value = "";
 
     }
 
+
+    function get_age(born){
+        var now = new Date();
+        var birthday = new Date(now.getFullYear(),born.getMonth(),born.getDate());
+        if(now >=birthday)
+            return now.getFullYear() - born.getFullYear();
+        else 
+            return now.getFullYear()-born.getFullYear()-1;
+        
+    }
+    
+    console.log(get_age(new Date(2001,05-1,25)));
+
+    //false text content, true value
+    function compare_select_value(select_element, value, value_or_textContent) {
+        var retrn_value = null;
+        var percentage_find = 0;
+        for (i = 0; i < select_element.length; i++) {
+
+            var percentage_similarity = similarity(select_element.options[i].textContent, value);
+            if (value_or_textContent) percentage_similarity = similarity(select_element.options[i].value, value);
+
+            if (percentage_similarity > 0.5) {
+                if (percentage_similarity > percentage_find) {
+                    retrn_value = select_element.options[i].value;
+                    percentage_find = percentage_similarity
+                }
+            }
+
+        }
+        return retrn_value;
+    }
+    function similarity(s1, s2) {
+        var longer = s1;
+        var shorter = s2;
+        if (s1.length < s2.length) {
+            longer = s2;
+            shorter = s1;
+        }
+        var longerLength = longer.length;
+        if (longerLength == 0) {
+            return 1.0;
+        }
+        return (longerLength - editDistance(longer, shorter)) / parseFloat(longerLength);
+    }
+
+    function editDistance(s1, s2) {
+        s1 = s1.toLowerCase();
+        s2 = s2.toLowerCase();
+
+        var costs = new Array();
+        for (var i = 0; i <= s1.length; i++) {
+            var lastValue = i;
+            for (var j = 0; j <= s2.length; j++) {
+                if (i == 0)
+                    costs[j] = j;
+                else {
+                    if (j > 0) {
+                        var newValue = costs[j - 1];
+                        if (s1.charAt(i - 1) != s2.charAt(j - 1))
+                            newValue = Math.min(Math.min(newValue, lastValue),
+                                costs[j]) + 1;
+                        costs[j - 1] = lastValue;
+                        lastValue = newValue;
+                    }
+                }
+            }
+            if (i > 0)
+                costs[s2.length] = lastValue;
+        }
+        return costs[s2.length];
+    }
+
     function OnMessageListener(data, sender, sendResponse) {
-        console.log("chrome.runtime.onMessage: "+data.command);
-        switch(data.command) {
-            case "copy_licence_holders" :
+        console.log("chrome.runtime.onMessage: " + data.command);
+        switch (data.command) {
+            case "copy_licence_holders":
                 console.log("La je copie");
-                var text =  document.getElementById("competiteur").textContent;
-                console.log("text : ",text);
-                console.log("text2 : ",JSON.parse(text));
+                var text = document.getElementById("competiteur").textContent;
+                console.log("text : ", text);
+                console.log("text2 : ", JSON.parse(text));
                 document.getElementById("init").click();
 
                 sendResponse({
                     response: JSON.parse(text)
                 });
-                break; 
-            case "paste_licence_holder" :
-                
+                break;
+            case "paste_licence_holder":
+
                 localStorage.setItem('actual_user', JSON.stringify(data.user));
 
                 inject(data);
@@ -125,24 +201,24 @@ chrome.runtime.onMessage.addListener(OnMessageListener);
                     status: "ok",
                     response: "Adhérent copié avec succès."
                 });
-        
+
                 break;
 
-            case "sync_added" :
-                if(document.getElementById("sync")!= undefined){
+            case "sync_added":
+                if (document.getElementById("sync") != undefined) {
                     document.getElementById("ids").textContent = JSON.stringify(data.ids);
                     document.getElementById("sync").click();
 
                     sendResponse({
                         status: "ok",
                     });
-                }else{
+                } else {
                     sendResponse({
                         status: "nok",
                     });
                 }
-                
-                
+
+
                 break;
 
             default:
